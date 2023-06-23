8c3b5d7b38c4f174d9c5f247c838fdeaf2493144
Maletuka <mfmahalefele@outlook.com>
2023-06-23 10:00:02 -0700

    Random numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <time.h>
 /* program generating random number */
+
 /**
  * main - Entry point
- * 
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 int n;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n < 0)
