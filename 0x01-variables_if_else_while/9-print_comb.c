475a1bbbbaa5a52a51b4891ce164d81c79363577
Maletuka <mfmahalefele@outlook.com>
2023-06-23 13:52:52 -0700

    nested for loops

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,20 +5,16 @@
  */
 int main(void)
 {
-int i, j;
+int i;
 
 for (i = 0; i <= 9; i++)
 {
-for (j = 0; j <= 9; j++)
-{
 putchar('0' + i);
-putchar('0' + j);
 
-if (i != 9 || j != 9)
+if (i != 9)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 return (0);
