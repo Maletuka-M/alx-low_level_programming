28b27f700336a300a02af57547082fdb82d33cae
Maletuka <mfmahalefele@outlook.com>
2023-06-24 10:48:46 -0700

    abcd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = 'a';
+
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+return (0);
+}
