6704d5c50fed56ab210a8b6c005a6a69784f7600
Maletuka <mfmahalefele@outlook.com>
2023-06-25 05:19:20 -0700

    absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Absolute value computation
+ * @i : is an integer
+ * integer - the data type computed and returned
+ * Return: Absolute value
+ */
+int _abs(int i)
+{
+if (i >= 0)
+{
+return (i);
+}
+else
+{
+return (i * -1);
+}
