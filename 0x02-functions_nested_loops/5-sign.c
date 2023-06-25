1382f6bb722e0760c80817ee0476d603fc172da5
Maletuka <mfmahalefele@outlook.com>
2023-06-25 04:57:07 -0700

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,21 +2,24 @@
 /**
  * print_sign - Identifies whether the number is +, - or 0
  * Prints 1 if +ve, 0 if zero 0 and -1 if -ve
- * @n : is an interger
+ * @n : is an integer
  * Return: 1 for +ve, 0 for 0 and -1 for -ve
  */
 int print_sign(int n)
 {
 if (n > 0)
 {
+_putchar(43);
 return (1);
 }
 else if (n < 0)
 {
+_putchar(45);
 return (-1);
 }
 else
 {
+_putchar(48);
 return (0);
 }
 _putchar('\n');
