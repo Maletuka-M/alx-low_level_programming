48c88a88fa94eea665dd1618bb28dfc058172f6a
Maletuka <mfmahalefele@outlook.com>
2023-06-25 09:24:53 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,11 +7,25 @@
 void print_to_98(int n)
 {
 
+if (n <= 98)
+{
 for (; n <= 98; n++)
 {
-_putchar('0' + n / 10);
-_putchar('0' + n % 10);
+_putchar(n / 10 + '0');
+_putchar(n % 10 + '0');
+if (n != 98)
 {
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+_putchar(n / 10 + '0');
+_putchar(n % 10 + '0');
 if (n != 98)
 {
 _putchar(',');
