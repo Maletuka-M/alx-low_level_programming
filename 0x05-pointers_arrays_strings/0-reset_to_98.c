3d31c6764bb3289d4ca049a2a34e96da2a687550
Maletuka <mfmahalefele@outlook.com>
2023-06-27 12:15:21 -0700

    Pointers to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - Takes a pointer to an int as parameter and updates value to 98
+ * @n : pointer variable
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+*n = 98
+
+return (0);
+}
